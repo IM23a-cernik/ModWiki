9bf196114350be837a007c0f1b24df3f816c6597
KrisHD1337 <kriscerni@gmail.com>
2026-05-05 10:04:06 +0200

    Fix for page routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: "https://im23a-cernik.github.io",
-  base: "/ModWiki/",
+  base: "/ModWiki",
   output: "static",
   vite: {
     plugins: [tailwindcss()]
