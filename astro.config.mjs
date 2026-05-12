b7b0719b058f24464b28800742466a249e5c045e
KrisHD1337 <kriscerni@gmail.com>
2026-05-12 11:09:21 +0200

    Hopefully fixed Deployment styling on aws again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const base = process.env.ASTRO_BASE_PATH ?? '/ModWiki/';
 // https://astro.build/config
 export default defineConfig({
   site: "https://im23a-cernik.github.io",
-  base: "/ModWiki",
+  base,
+  outDir: "../dist",
   output: "static",
   vite: {
     plugins: [tailwindcss()]
