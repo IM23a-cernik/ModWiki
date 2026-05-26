317b5e66cdc7d2a9107d10617a9221929d676984
KrisHD1337 <kriscerni@gmail.com>
2026-05-26 09:51:49 +0200

    #5 Added bug reporting functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -10,6 +11,12 @@ export default defineConfig({
   base,
   outDir: "./dist",
   output: "static",
+  security: {
+    checkOrigin: false
+  },
+  adapter: node({
+    mode: "standalone"
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
